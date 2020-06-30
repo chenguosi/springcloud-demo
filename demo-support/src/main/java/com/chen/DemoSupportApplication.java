dac067f1c9d6066728a65985bb3b220eb193fe53
GusChan <chenguosi@qq.com>
2020-06-30 10:43:57 +0800

    初始化support和content服务

---
+++ demo-support/src/main/java/com/chen/DemoSupportApplication.java
@@ -0,0 +1,15 @@
+package com.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class DemoSupportApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSupportApplication.class, args);
+    }
+    
+}
