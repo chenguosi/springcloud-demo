9ba88439c61d09a6c6bebd4de12bb67d4f5571ce
GusChan <chenguosi@qq.com>
2020-06-30 10:00:22 +0800

    创建注册中心、配置中心和公用API

---
+++ demo-config/src/main/java/com/chen/DemoConfigApplication.java
@@ -0,0 +1,17 @@
+package com.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class DemoConfigApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DemoConfigApplication.class, args);
+    }
+    
+}
